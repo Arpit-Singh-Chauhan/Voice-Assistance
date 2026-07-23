b38f6aab529212db0e30267f6633dd315910a32a
Arpit-Singh-Chauhan <arpitbrajesh12@gmail.com>
2026-07-23 23:59:10 +0530

    more songs

--- music_lib.py
+++ music_lib.py
@@ -8,5 +8,8 @@ music = {
     "aadat": "https://youtu.be/e5LShHAE03A?si=CsqaSwx3Xfne7HG6",
     "tum se hi": "https://youtu.be/Cb6wuzOurPc?si=LB14Rdqi27v5sfhz",
     "barso ke intezar ka": "https://youtu.be/PFxomXeIq4w?si=W9hghio99DEkHVND",
-
+    "man without love": "https://youtu.be/S2ujotDMluo?si=tC8DsaotK-Tmz73H",
+    "zaalima": "https://youtu.be/lpdRqn6xwiM?si=jELm_qcWj915q_VY",
+    "zara sa": "https://youtu.be/-8C_2BBVWk8?si=JfJijUQtiFIZfHc0",
+    "in dino": "https://youtu.be/dXFVOkq41us?si=8hpFckb0VsnDvwCX"
 }
